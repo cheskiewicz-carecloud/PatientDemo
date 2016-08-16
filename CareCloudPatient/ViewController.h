5ceabe99913416afb39c0bd91ac0243f94911942
cheskiewicz <chuckc@appzooka.com>
2016-08-16 12:39:10 -0400

    Initial Commit

---
+++ CareCloudPatient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CareCloudPatient
+//
+//  Created by cheskiewicz on 8/16/16.
+//  Copyright © 2016 CareCloud Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CareCloudPatient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CareCloudPatient
+//
+//  Created by cheskiewicz on 8/16/16.
+//  Copyright © 2016 CareCloud Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
